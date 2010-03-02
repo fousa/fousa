016cc3e34f00e2f550f1395f012a5aabc77476ca
Jelle Vandebeeck <jelle@fousa.be>
2010-03-02 09:31:00 +0100

    Fixed another error in the migrations

--- db/migrate/20090114160315_add_note_to_post.rb
+++ db/migrate/20090114160315_add_note_to_post.rb
@@ -2,7 +2,7 @@ class AddNoteToPost < ActiveRecord::Migration
   def self.up
     add_column :posts, :note, :boolean
 
-    ActiveRecord::Base.connection().update_sql("UPDATE posts SET note = f")
+    ActiveRecord::Base.connection().update_sql("UPDATE posts SET note = 'f'")
   end
 
   def self.down
